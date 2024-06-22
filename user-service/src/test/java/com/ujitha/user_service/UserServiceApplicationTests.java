c91bcc8ae86b3047abac402a36eeeae43b71e610
Ujitha <ujithamigara@gmail.com>
2024-06-22 17:11:12 +0530

    add user-service

---
+++ user-service/src/test/java/com/ujitha/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujitha.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
