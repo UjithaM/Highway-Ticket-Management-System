250187a844b0829df9c0e98afb0d8432296d66e2
Ujitha <ujithamigara@gmail.com>
2024-06-22 13:52:56 +0530

    add config-server

---
+++ config-server/src/test/java/com/ujithamigara/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujithamigara.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
