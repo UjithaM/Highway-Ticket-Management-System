f20dcf37e4c6a31398b4d8ef2e9f616cd8e95b4c
Ujitha <ujithamigara@gmail.com>
2024-06-24 11:21:20 +0530

    add UserEntity

---
+++ user-service/src/main/java/com/ujitha/user_service/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.ujitha.user_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "user")
+@Entity
+public class UserEntity implements SuperEntity{
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
