6462f3e3737ea30a87c84d0f512f790c3757e59c
Ujitha <ujithamigara@gmail.com>
2024-06-24 11:33:32 +0530

    implement CRUD

--- user-service/src/main/java/com/ujitha/user_service/service/impl/UserServiceIMPL.java
+++ user-service/src/main/java/com/ujitha/user_service/service/impl/UserServiceIMPL.java
@@ -2,7 +2,9 @@ package com.ujitha.user_service.service.impl;
 
 import com.ujitha.user_service.dao.UserRepo;
 import com.ujitha.user_service.dto.UserDTO;
+import com.ujitha.user_service.entity.UserEntity;
 import com.ujitha.user_service.service.UserService;
+import com.ujitha.user_service.util.Mapping;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,29 +16,39 @@ import java.util.List;
 public class UserServiceIMPL implements UserService {
 
     private final UserRepo userRepo;
+    private final Mapping mapping;
 
     @Override
     public UserDTO saveUser(UserDTO userDTO) {
-        return null;
+        return mapping.toUserDto(userRepo.save(mapping.toUserEntity(userDTO)));
     }
 
     @Override
     public void deleteUser(String userId) {
-
+        userRepo.deleteById(userId);
     }
 
     @Override
     public UserDTO getSelectedUser(String userId) {
-        return null;
+        return mapping.toUserDto(userRepo.findById(userId).orElse(null));
     }
 
     @Override
     public List<UserDTO> getAllUser() {
-        return List.of();
+        return mapping.toUserDTOList(userRepo.findAll());
     }
 
     @Override
     public void updateUser(String userId, UserDTO userDTO) {
+        UserEntity userEntity = userRepo.findById(userId).orElse(null);
+
+        if (userEntity != null) {
+            userEntity.setFirstName(userDTO.getFirstName());
+            userEntity.setLastName(userDTO.getLastName());
+            userEntity.setEmail(userDTO.getEmail());
+            userEntity.setPassword(userDTO.getPassword());
+            userRepo.save(userEntity);
+        }
 
     }
 }
