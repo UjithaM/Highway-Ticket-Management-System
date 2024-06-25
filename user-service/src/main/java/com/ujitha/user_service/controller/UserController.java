982b3541172c0ec360bd81e78a235db6c34601fa
Ujitha <ujithamigara@gmail.com>
2024-06-25 21:19:29 +0530

    implement user health check

--- user-service/src/main/java/com/ujitha/user_service/controller/UserController.java
+++ user-service/src/main/java/com/ujitha/user_service/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.ujitha.user_service.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping("/health")
+    public String healthTest() {
+        return "User Health Test";
+    }
 }
