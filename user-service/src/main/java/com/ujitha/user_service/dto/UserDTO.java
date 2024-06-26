04781eb277ce9f3cbbe41b7bc0bab74eefb6a7ab
Ujitha <ujithamigara@gmail.com>
2024-06-26 11:47:13 +0530

    add validations

--- user-service/src/main/java/com/ujitha/user_service/dto/UserDTO.java
+++ user-service/src/main/java/com/ujitha/user_service/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.ujitha.user_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserDTO {
     private String id;
+    @NotBlank(message = "User First name cannot be blank")
     private String firstName;
+    @NotBlank(message = "User Last name cannot be blank")
     private String lastName;
+    @NotBlank(message = "User Email name cannot be blank")
     private String email;
+    @NotBlank(message = "User Password name cannot be blank")
     private String password;
 }
