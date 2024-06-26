a73200bd09c1d1d8869990e22209fc76d4c0c3ac
Ujitha <ujithamigara@gmail.com>
2024-06-26 13:03:18 +0530

    implement get all users

--- user-service/src/main/java/com/ujitha/user_service/controller/UserController.java
+++ user-service/src/main/java/com/ujitha/user_service/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ujitha.user_service.controller;
 import com.ujitha.user_service.dto.UserDTO;
 import com.ujitha.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -40,6 +43,19 @@ public class UserController {
             logger.error("Error saving User: ", exception);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Internal server error | User saved Unsuccessfully.\nMore Details\n" + exception);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() {
+        logger.info("Fetching all Users");
+        try {
+            List<UserDTO> userDTOList = userService.getAllUser();
+            return ResponseEntity.ok(userDTOList);
+        } catch (Exception exception) {
+            logger.error("Error fetching all Users: ", exception);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Internal server error | Unable to fetch Users.\nMore Details\n" + exception);
         }
     }
 }
