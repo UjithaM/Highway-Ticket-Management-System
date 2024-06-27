6d40f28add45733375fa057c7134317049c0a848
Ujitha <ujithamigara@gmail.com>
2024-06-27 12:04:56 +0530

    add model mapper

--- vehicle-service/src/main/java/com/ujitha/vehicle_service/VehicleServiceApplication.java
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/VehicleServiceApplication.java
@@ -1,13 +1,18 @@
 package com.ujitha.vehicle_service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class VehicleServiceApplication {
-
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleServiceApplication.class, args);
 	}
