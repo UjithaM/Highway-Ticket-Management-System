b2b3a843dfb7ae640572bbcbffbdb03b34884bd2
Ujitha <ujithamigara@gmail.com>
2024-06-27 21:47:16 +0530

    add ResetTemplete

--- vehicle-service/src/main/java/com/ujitha/vehicle_service/VehicleServiceApplication.java
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/VehicleServiceApplication.java
@@ -4,7 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,6 +17,12 @@ public class VehicleServiceApplication {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
