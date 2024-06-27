7cb205773d9c3b965398b139c7f5d5c16af1213a
Ujitha <ujithamigara@gmail.com>
2024-06-27 12:34:14 +0530

    implement lombok getters and setters

--- vehicle-service/src/main/java/com/ujitha/vehicle_service/dto/VehicleDTO.java
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/dto/VehicleDTO.java
@@ -1,5 +1,12 @@
 package com.ujitha.vehicle_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class VehicleDTO {
     private String vehicleId;
     private String licensePlate;
