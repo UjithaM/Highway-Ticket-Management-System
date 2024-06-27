771af359de9cd3cc0dca368f9acdb2b6b30965ad
Ujitha <ujithamigara@gmail.com>
2024-06-27 12:35:04 +0530

    implement all

--- vehicle-service/src/main/java/com/ujitha/vehicle_service/service/impl/VehicleServiceIMPL.java
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/service/impl/VehicleServiceIMPL.java
@@ -1,7 +1,11 @@
 package com.ujitha.vehicle_service.service.impl;
 
+import com.ujitha.vehicle_service.dao.VehicleRepo;
 import com.ujitha.vehicle_service.dto.VehicleDTO;
+import com.ujitha.vehicle_service.entity.VehicleEntity;
 import com.ujitha.vehicle_service.service.VehicleService;
+import com.ujitha.vehicle_service.util.Mapping;
+import com.ujitha.vehicle_service.util.exception.NotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,28 +15,52 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class VehicleServiceIMPL implements VehicleService {
+
+    private final VehicleRepo vehicleRepo;
+    private final Mapping mapping;
+
     @Override
     public VehicleDTO saveVehicle(VehicleDTO vehicleDTO) {
-        return null;
+        long vehicleCount = vehicleRepo.count();
+        String vehicleId = String.format("V%04d", vehicleCount + 1);
+        vehicleDTO.setVehicleId(vehicleId);
+        return mapping.toVehicleDTO(vehicleRepo.save(mapping.toVehicleEntity(vehicleDTO)));
     }
 
     @Override
     public void deleteVehicle(String vehicleId) {
-
+        if (vehicleRepo.existsById(vehicleId)){
+            vehicleRepo.deleteById(vehicleId);
+        } else throw new NotFoundException("Vehicle not found" + vehicleId);
     }
 
     @Override
     public VehicleDTO getSelectedVehicle(String vehicleId) {
-        return null;
+        VehicleEntity vehicleEntity = vehicleRepo.findById(vehicleId)
+                .orElseThrow(() -> new NotFoundException("Vehicle not found" + vehicleId));
+        return mapping.toVehicleDTO(vehicleEntity);
     }
 
     @Override
     public List<VehicleDTO> getAllVehicle() {
-        return null;
+        return mapping.toVehicleDTOList(vehicleRepo.findAll());
     }
 
     @Override
     public void updateVehicle(String vehicleId, VehicleDTO vehicleDTO) {
+        VehicleEntity vehicleEntity = vehicleRepo.findById(vehicleId)
+                .orElseThrow(() -> new NotFoundException("Vehicle not found" + vehicleId));
+        if (vehicleEntity != null) {
+            vehicleEntity.setLicensePlate(vehicleDTO.getLicensePlate());
+            vehicleEntity.setBrand(vehicleDTO.getBrand());
+            vehicleEntity.setModel(vehicleDTO.getModel());
+            vehicleEntity.setUserId(vehicleDTO.getUserId());
+            vehicleRepo.save(vehicleEntity);
+        }
+    }
 
+    @Override
+    public List<VehicleDTO> getVehicleByUserId(String userId) {
+        return mapping.toVehicleDTOList(vehicleRepo.findByUserId(userId));
     }
 }
