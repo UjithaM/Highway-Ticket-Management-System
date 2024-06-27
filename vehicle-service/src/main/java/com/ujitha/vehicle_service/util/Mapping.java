4337f922ca72f9ea45b7677c354b032ac824a985
Ujitha <ujithamigara@gmail.com>
2024-06-27 12:02:43 +0530

    add mapping and implement

---
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/util/Mapping.java
@@ -0,0 +1,31 @@
+package com.ujitha.vehicle_service.util;
+
+import com.ujitha.vehicle_service.dto.VehicleDTO;
+import com.ujitha.vehicle_service.entity.VehicleEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class Mapping {
+    private final ModelMapper mapper;
+    Mapping(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public VehicleDTO toVehicleDTO (VehicleEntity vehicleEntity) {
+        return mapper.map(vehicleEntity, VehicleDTO.class);
+    }
+
+    public VehicleEntity toVehicleEntity (VehicleDTO vehicleDTO) {
+        return mapper.map(vehicleDTO, VehicleEntity.class);
+    }
+
+    public List<VehicleDTO> toVehicleDTOList(List<VehicleEntity> vehicleEntities) {
+        return vehicleEntities.stream()
+                .map(vehicleEntity -> mapper.map(vehicleEntity, VehicleDTO.class))
+                .collect(Collectors.toList());
+    }
+}
