008d46d0ca5ed4fef49ac5fd366e84058c2a0098
Ujitha <ujithamigara@gmail.com>
2024-06-27 12:03:50 +0530

    add vehicleDTO

---
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/dto/VehicleDTO.java
@@ -0,0 +1,9 @@
+package com.ujitha.vehicle_service.dto;
+
+public class VehicleDTO {
+    private String vehicleId;
+    private String licensePlate;
+    private String brand;
+    private String model;
+    private String userId;
+}
