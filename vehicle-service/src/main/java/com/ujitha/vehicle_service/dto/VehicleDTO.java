bce8d8f48c5740ea7dcc60419eb05bcebb7b0b52
Ujitha <ujithamigara@gmail.com>
2024-06-27 21:46:34 +0530

    add validation

--- vehicle-service/src/main/java/com/ujitha/vehicle_service/dto/VehicleDTO.java
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/dto/VehicleDTO.java
@@ -1,5 +1,6 @@
 package com.ujitha.vehicle_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 @Data
 public class VehicleDTO {
     private String vehicleId;
+    @NotBlank(message = "Vehicle License plate cannot be blank")
     private String licensePlate;
+    @NotBlank(message = "Vehicle Brand cannot be blank")
     private String brand;
+    @NotBlank(message = "Vehicle Model cannot be blank")
     private String model;
+    @NotBlank(message = "Vehicle User ID cannot be blank")
     private String userId;
 }
