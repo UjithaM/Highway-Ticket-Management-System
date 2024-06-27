b528195d57cd51df7474de4548a3666166fac56f
Ujitha <ujithamigara@gmail.com>
2024-06-27 11:39:35 +0530

    add VehicleController and implement health check

---
+++ vehicle-service/src/main/java/com/ujitha/vehicle_service/controller/VehicleController.java
@@ -0,0 +1,17 @@
+package com.ujitha.vehicle_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/vehicle")
+@RequiredArgsConstructor
+public class VehicleController {
+
+    @GetMapping("/health")
+    public String healthTest() {
+        return "Vehicle Health Test";
+    }
+}
