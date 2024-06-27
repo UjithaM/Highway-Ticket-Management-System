008a2047edade6369d879203eecea65cf42d62b0
Ujitha <ujithamigara@gmail.com>
2024-06-27 11:28:11 +0530

    add custom exception and implement it

---
+++ user-service/src/main/java/com/ujitha/user_service/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ujitha.user_service.util.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
