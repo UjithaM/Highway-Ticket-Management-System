829a4071edb07ed87a449b1904abb5bcd2f32dcf
Ujitha <ujithamigara@gmail.com>
2024-06-28 09:38:29 +0530

    add TicketEntity

---
+++ ticket-service/src/main/java/com/ujithamigara/ticket_service/entity/TicketEntity.java
@@ -0,0 +1,24 @@
+package com.ujithamigara.ticket_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "vehicle")
+@Entity
+public class TicketEntity implements SuperEntity{
+    @Id
+    private String ticketId;
+    private Date ticketIssueDate;
+    private String ticketStatus;
+    private String vehicleId;
+    private String userId;
+}
