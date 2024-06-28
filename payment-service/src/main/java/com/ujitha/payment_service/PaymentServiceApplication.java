892c040fe49c887c567a15c07bea39869bbb9db1
Ujitha <ujithamigara@gmail.com>
2024-06-28 11:36:01 +0530

    add modelMapper and RestTemplate

--- payment-service/src/main/java/com/ujitha/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/ujitha/payment_service/PaymentServiceApplication.java
@@ -1,8 +1,12 @@
 package com.ujitha.payment_service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +14,17 @@ public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
